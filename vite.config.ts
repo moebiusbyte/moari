7aa62b86d65b37a6b9a10f173c26342bd5fcb106
Joao <coderjao@hotmail.com>
2025-02-04 09:13:32 -0300

    rebase

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
-  import react from "@vitejs/plugin-react";
-  
-  // https://vitejs.dev/config/
-  export default defineConfig({
-      plugins: [react()],
-  });
-  
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    allowedHosts: [
+      "localhost",
+      "v3rks3-5173.csb.app",
+      ".csb.app", // Isso permitirá todos os subdomínios do csb.app
+    ],
+  },
+});
