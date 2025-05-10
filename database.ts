1f458f64e69bcf383194b19f47e929b34e076d60
Maira Melo <maira.melo26@gmail.com>
2025-05-09 23:18:12 -0300

    fix: correção da porta do banco e nome do container não o ip

--- database.ts
+++ database.ts
@@ -6,7 +6,7 @@ import dotenv from 'dotenv';
 dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
 // Usa o DATABASE_URL do ambiente ou define um valor padrão para o Docker
-const databaseUrl = process.env.DATABASE_URL || 'postgresql://postgres:152615@postgres-container:5433/moari';
+const databaseUrl = process.env.DATABASE_URL || 'postgresql://postgres:152615@postgres-container:5432/moari';
 
 // Criar pool de conexões
 const pool = new Pool({
