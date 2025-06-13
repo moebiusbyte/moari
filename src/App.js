ec7e44d1a98ce2086443d2ec8231c60359c38453
Joao <coderjao@hotmail.com>
2025-06-13 17:41:30 -0300

    fix: build estável

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import { jsx as _jsx } from "react/jsx-runtime";
+import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import Layout from "./Layout";
 //import DataAtual from ",/DataAtual.tsx"
 export default function App() {
-    return (
-    // AuthProvider envolve toda a aplicação para que o estado de autenticação
-    // esteja disponível em todos os componentes
-    _jsx(AuthProvider, { children: _jsx(BrowserRouter, { children: _jsx(Layout, {}) }) }));
+    return (_jsxs("div", { children: [_jsx("div", { className: "bg-red-500 p-8 text-white text-center text-xl font-bold", children: "\uD83C\uDF89 TESTE TAILWIND \uD83C\uDF89" }), _jsx(AuthProvider, { children: _jsx(BrowserRouter, { children: _jsx(Layout, {}) }) })] }));
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "./styles.css";
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
 root.render(_jsx(React.StrictMode, { children: _jsx(App, {}) }));
